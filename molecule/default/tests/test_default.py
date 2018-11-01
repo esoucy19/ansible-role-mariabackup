3aefb1c7cecb80b048bbb74d2d54831c84ffcdd9
Etienne Soucy <esoucy@bigtek.ca>
2018-11-01 15:25:18 -0400

    Passing test MariaDB version, enabled and running

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -15,4 +15,20 @@ def test_hosts_file(host):
 
 
 def test_mariadb_installed(host):
-    assert host.package("mariadb-server").is_installed
+    p = host.package('MariaDB-server')
+    assert p.is_installed
+
+    version = tuple(map(int, p.version.split('-')[0].split('.')))
+    version_error = "MariaDB version must be at least 10.1.23 or 10.2.7"
+    assert version[0] >= 10, version_error
+    assert version[1] >= 1, version_error
+    if version[1] == 1:
+        assert version[2] >= 23, version_error
+    if version[1] == 2:
+        assert version[2] >= 7, version_error
+
+
+def test_mariadb_running(host):
+    s = host.service('mysql')
+    assert s.is_running
+    assert s.is_enabled
