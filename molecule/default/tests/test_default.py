33f230b57ccaa5bf7ba6ef9e674f79ede219f9d2
Etienne Soucy <esoucy@bigtek.ca>
2018-11-01 15:26:18 -0400

    Refactor: removed dummy hosts file test from test

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,14 +6,6 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
-
-
 def test_mariadb_installed(host):
     p = host.package('MariaDB-server')
     assert p.is_installed
