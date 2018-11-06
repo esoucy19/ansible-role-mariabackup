3b78504f8159d82507bd19087882941f0e2f3fd3
Etienne Soucy <esoucy@bigtek.ca>
2018-11-06 09:56:14 -0500

    red: test_user_mariabackup_mycnf

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -39,3 +39,14 @@ def test_mariabackup_installed(host):
 def test_user_mariabackup_exists(host, vars):
     mariabackup_user = host.user(vars['mariabackup_user'])
     assert mariabackup_user.exists
+
+
+def test_user_mariabackup_mycnf(host, vars):
+    mycnf = host.file(''.join(['/home/', vars['mariabackup_user'], '.my.cnf']))
+    assert mycnf.exists
+    assert mycnf.user == vars['mariabackup_user']
+    assert mycnf.group == vars['mariabackup_user']
+    assert oct(mycnf.mode) == '0600'
+    assert mycnf.contains(''.join(['user=', vars['mariabackup_mysql_user']]))
+    assert mycnf.contains(''.join(['password=',
+                                   vars['mariabackup_mysql_password']]))
